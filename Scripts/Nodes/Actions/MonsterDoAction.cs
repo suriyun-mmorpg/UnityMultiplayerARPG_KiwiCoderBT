a114015cb08ebf542fc408dc3437a0df2257898f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-05-21 00:29:40 +0700

    Split character turning and do action nodes

--- Scripts/Nodes/Actions/MonsterDoAction.cs
+++ Scripts/Nodes/Actions/MonsterDoAction.cs
@@ -4,12 +4,11 @@ namespace MultiplayerARPG.KiwiCoderBT
 {
     public class MonsterDoAction : MonsterActionNode
     {
-        [Tooltip("Turn to enemy speed")]
-        public float turnToEnemySpeed = 800f;
+        private bool didAction;
 
         protected override void OnStart()
         {
-
+            didAction = false;
         }
 
         protected override void OnStop()
@@ -18,6 +17,12 @@ namespace MultiplayerARPG.KiwiCoderBT
 
         protected override State OnUpdate()
         {
+            if (didAction && Entity.IsPlayingActionAnimation())
+            {
+                ClearActionState();
+                return State.Success;
+            }
+
             IDamageableEntity tempTargetEnemy;
             if (!Entity.TryGetTargetEntity(out tempTargetEnemy) || Entity.Characteristic == MonsterCharacteristic.NoHarm)
             {
@@ -32,27 +37,6 @@ namespace MultiplayerARPG.KiwiCoderBT
                 Entity.SetTargetEntity(null);
                 ClearActionState();
                 return State.Failure;
-            }
-
-            Vector3 currentPosition = Entity.CacheTransform.position;
-            Vector3 targetPosition = tempTargetEnemy.GetTransform().position;
-            Vector3 lookAtDirection = (targetPosition - currentPosition).normalized;
-            if (lookAtDirection.sqrMagnitude > 0)
-            {
-                if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-                {
-                    Quaternion currentLookAtRotation = Entity.GetLookRotation();
-                    Vector3 lookRotationEuler = Quaternion.LookRotation(lookAtDirection).eulerAngles;
-                    lookRotationEuler.x = 0;
-                    lookRotationEuler.z = 0;
-                    currentLookAtRotation = Quaternion.RotateTowards(currentLookAtRotation, Quaternion.Euler(lookRotationEuler), turnToEnemySpeed * Time.deltaTime);
-                    Entity.SetLookRotation(currentLookAtRotation);
-                }
-                else
-                {
-                    // Update 2D direction
-                    Entity.SetLookRotation(Quaternion.LookRotation(lookAtDirection));
-                }
             }
 
             Entity.AimPosition = Entity.GetAttackAimPosition(ref blackboard.isLeftHandAttacking);
@@ -74,8 +58,8 @@ namespace MultiplayerARPG.KiwiCoderBT
                 Entity.Attack(false);
             }
 
-            ClearActionState();
-            return State.Success;
+            didAction = true;
+            return State.Running;
         }
     }
 }
---
+++ Scripts/Nodes/Actions/MonsterTurnToEnemy.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+namespace MultiplayerARPG.KiwiCoderBT
+{
+    public class MonsterTurnToEnemy : MonsterActionNode
+    {
+        [Tooltip("Turn to enemy speed")]
+        public float turnToEnemySpeed = 800f;
+        [Min(1f)]
+        public float successAngle = 10f;
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+            IDamageableEntity tempTargetEnemy;
+            if (!Entity.TryGetTargetEntity(out tempTargetEnemy) || Entity.Characteristic == MonsterCharacteristic.NoHarm)
+            {
+                // No target, stop attacking
+                ClearActionState();
+                return State.Failure;
+            }
+
+            if (tempTargetEnemy.Entity == Entity.Entity || tempTargetEnemy.IsHideOrDead() || !tempTargetEnemy.CanReceiveDamageFrom(Entity.GetInfo()))
+            {
+                // If target is dead or in safe area stop attacking
+                Entity.SetTargetEntity(null);
+                ClearActionState();
+                return State.Failure;
+            }
+
+            Vector3 currentPosition = Entity.CacheTransform.position;
+            Vector3 targetPosition = tempTargetEnemy.GetTransform().position;
+            Vector3 lookAtDirection = (targetPosition - currentPosition).normalized;
+            if (lookAtDirection.sqrMagnitude > 0)
+            {
+                if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
+                {
+                    Quaternion currentLookAtRotation = Entity.GetLookRotation();
+                    Vector3 lookRotationEuler = Quaternion.LookRotation(lookAtDirection).eulerAngles;
+                    lookRotationEuler.x = 0;
+                    lookRotationEuler.z = 0;
+                    Quaternion nextLookAtRotation = Quaternion.RotateTowards(currentLookAtRotation, Quaternion.Euler(lookRotationEuler), turnToEnemySpeed * Time.deltaTime);
+                    Entity.SetLookRotation(nextLookAtRotation);
+
+                    if (Quaternion.Angle(currentLookAtRotation, nextLookAtRotation) >= successAngle)
+                        return State.Running;
+                }
+                else
+                {
+                    // Update 2D direction
+                    Entity.SetLookRotation(Quaternion.LookRotation(lookAtDirection));
+                    return State.Success;
+                }
+            }
+            return State.Success;
+        }
+    }
+}
