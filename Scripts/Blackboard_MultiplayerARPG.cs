1a3a3f579574a8cee4c0811786c2f88b27bf9a63
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-05-20 20:27:28 +0700

    Add some variable to blackboard

---
+++ Scripts/Blackboard_MultiplayerARPG.cs
@@ -0,0 +1,14 @@
+using MultiplayerARPG;
+using MultiplayerARPG.KiwiCoderBT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheKiwiCoder
+{
+    public partial class Blackboard
+    {
+        public MonsterActivityComponentBT activityComp;
+        public DamageableEntity target;
+    }
+}
